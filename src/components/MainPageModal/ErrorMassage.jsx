f09011cb2d393c668c88e94d216ffb841657bd12
Artur Myasnikov <102307516+ArturMyasnikov@users.noreply.github.com>
2022-12-11 19:25:33 -0800

    fix

---
+++ src/components/MainPageModal/ErrorMassage.jsx
@@ -0,0 +1,5 @@
+import s from './mainPageModal.module.css';
+
+export default function ErrorMassage() {
+	return <span className={s.errorMassage}>This is a required field</span>;
+}
--- src/components/MainPageModal/MainPageModal.jsx
+++ src/components/MainPageModal/MainPageModal.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import ErrorMassage from './ErrorMassage';
 import s from './mainPageModal.module.css';
 
 const defaultValues = { email: '', password: '' };
 
 export default function MainPageModal() {
 	const [user, setUser] = useState(defaultValues);
+	const [error, setError] = useState(false);
 
 	const handleChange = event => {
 		const { name, value } = event.target;
@@ -13,6 +16,9 @@ export default function MainPageModal() {
 	};
 
 	const onLogin = event => {
+		if (!user.email) {
+			setError(true);
+		}
 		localStorage.setItem('user', JSON.stringify(user));
 		setUser(defaultValues);
 	};
@@ -22,41 +28,7 @@ export default function MainPageModal() {
 			<div className={s.modal}>
 				<p className={s.googleText}>You can log in with your Google Account:</p>
 				<button type="button" className={s.googleBtn}>
-					<svg
-						stroke="currentColor"
-						fill="currentColor"
-						stroke-width="0"
-						version="1.1"
-						x="0px"
-						y="0px"
-						viewBox="0 0 48 48"
-						enable-background="new 0 0 48 48"
-						height="18"
-						width="18"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							fill="#FFC107"
-							d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
-	c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
-	c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-						></path>
-						<path
-							fill="#FF3D00"
-							d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
-	C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-						></path>
-						<path
-							fill="#4CAF50"
-							d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
-	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-						></path>
-						<path
-							fill="#1976D2"
-							d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
-	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-						></path>
-					</svg>
+					<FcGoogle size={18} />
 					<span className={s.googleBtnText}>Google</span>
 				</button>
 				<p className={s.loginText}>
@@ -75,9 +47,7 @@ export default function MainPageModal() {
 							title="This is a required field"
 							required
 						/>
-						{defaultValues && (
-							<span className={s.errorMassage}>This is a required field</span>
-						)}
+						{error && <ErrorMassage error={error} />}
 					</label>
 					<label className={s.label}>
 						Password :
@@ -89,7 +59,7 @@ export default function MainPageModal() {
 							placeholder="password"
 							required
 						/>
-						<span className={s.errorMassage}>This is a required field</span>
+						{error && <ErrorMassage />}
 					</label>
 					<div className={s.btnWrapper}>
 						<button type="submit" className={s.modalBtn} onClick={onLogin}>
